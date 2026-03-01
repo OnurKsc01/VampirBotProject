4b092897731c84a8b61cd2ef5daf60a1686db6c9
OnurKsc01 <onurhasking@gmail.com>
2026-03-01 23:08:42 +0300

    Update Main.java

--- src/main/java/com/vampiroyunu/Main.java
+++ src/main/java/com/vampiroyunu/Main.java
@@ -1,21 +1,41 @@
 package com.vampiroyunu;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class Main {
     public static void main(String[] args) {
+        
+        // 1. KISIM: RENDER'I KANDIRAN SAHTE WEB SUNUCUSU
         try {
-            // Telegram API'sine bağlanıyoruz
+            // Render'ın bize verdiği gizli portu buluyoruz (Yoksa 8080 kullanıyoruz)
+            String portStr = System.getenv("PORT");
+            int port = (portStr != null) ? Integer.parseInt(portStr) : 8080;
+            
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.createContext("/", exchange -> {
+                String response = "Vampir Bot 7/24 Aktif ve Calisiyor!";
+                exchange.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = exchange.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+            });
+            server.start();
+            System.out.println("Sahte web sunucusu port " + port + " uzerinde basladi. Render artik mutlu!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 2. KISIM: ASIL VAMPİR BOTUMUZU BAŞLATIYORUZ
+        try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            
-            // Kendi yazdığımız botu sisteme kaydediyoruz
             botsApi.registerBot(new VampirBot());
-            
             System.out.println("Yakamoz'un Vampir Botu başarıyla çalıştırıldı! Kanlı gece başlıyor...");
-            
-        } catch (TelegramApiException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
